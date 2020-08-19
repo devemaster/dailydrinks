34cb5c77c4cf6ccb75b3fb70f0a0569b89a6e5ae
kapilkarda <kkarda77@gmail.com>
2020-08-19 14:57:18 +0530

    changes

--- db.js
+++ db.js
@@ -1,15 +1,22 @@
 const { Client } = require('pg');
 
+// const client = new Client({
+// 	user: process.env.DB_USERNAME,
+// 	password: process.env.DB_PASSWORD,
+// 	host: process.env.DB_HOST,
+// 	port: process.env.DB_PORT,
+// 	database: 'redis',
+// 	ssl: true
+// })
+
 const client = new Client({
-	user: process.env.DB_USERNAME,
-	password: process.env.DB_PASSWORD,
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
+	user: 'postgres',
+	password: 'Emaster@123',
+	host: 'localhost',
+	port: 5432,
 	database: 'redis',
 	ssl: true
 })
-
-
 client
 	.connect()
 	.then(() => console.log('connected to DB'))
